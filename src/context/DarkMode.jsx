4f4ff8c434d9bac3b74c24f8ca5c5484cf171e38
ConquerorFY <ryanrlfy33@gmail.com>
2026-04-27 01:08:33 +0800

    feat: modifed dark mode logic

--- src/context/DarkMode.jsx
+++ src/context/DarkMode.jsx
@@ -2,31 +2,21 @@ import { useState, useEffect } from 'react';
 import { DarkModeContext } from '../hooks/useDarkMode';
 
 const DarkModeProvider = ({ children }) => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(localStorage.getItem('theme')
+    ? localStorage.getItem('theme') === 'dark'
+    : window.matchMedia('(prefers-color-scheme: dark)').matches);
 
   useEffect(() => {
-    // On page load or when changing themes, best to add inline in `head` to avoid FOUC
-    if (
-      localStorage.getItem('theme') === 'dark' ||
-      (!localStorage.getItem('theme') &&
-        window.matchMedia('(prefers-color-scheme: dark)').matches)
-    ) {
-      document.documentElement.classList.add('dark');
-      setIsDarkMode(true);
-    } else {
-      document.documentElement.classList.remove('dark');
-      setIsDarkMode(false);
-    }
-  }, []);
-
-  const toggleDarkMode = () => {
     if (isDarkMode) {
-      document.documentElement.classList.remove('dark');
-      localStorage.setItem('theme', 'light');
-    } else {
       document.documentElement.classList.add('dark');
       localStorage.setItem('theme', 'dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
     }
+  }, [isDarkMode])
+
+  const toggleDarkMode = () => {
     setIsDarkMode(!isDarkMode);
   };
 
