e123015efe89f6b1f4e0b6be6a24e353d477152a
ConquerorFY <ryanrlfy33@gmail.com>
2026-04-27 10:15:53 +0800

    feat: breakdown constants

---
+++ src/constants/projects.js
@@ -0,0 +1,65 @@
+import {
+  socialmedia,
+  game,
+  extension,
+  player,
+  // consultation,
+  // face,
+} from '../assets/icons';
+
+export const projects = [
+  {
+    iconUrl: socialmedia,
+    theme: 'btn-back-red',
+    name: 'Moosan Durian Club',
+    description:
+      "In this project, I'm responsible for developing the frontend UIs using HTML, CSS and NextJS. I'm also responsible for integrating APIs from the backend using React Query.",
+    link: 'https://moosan.club/',
+  },
+  {
+    iconUrl: game,
+    theme: 'btn-back-green',
+    name: 'Unilah Overseas Campus (UOC)',
+    description:
+      "In this project, I'm responsible for developing the frontend UIs using HTML, CSS and React. I'm also responsible for integrating APIs from the NestJS backend.",
+    link: 'https://uoc.myunilah.com/',
+  },
+  {
+    iconUrl: extension,
+    theme: 'btn-back-blue',
+    name: 'OnLine Academy',
+    description:
+      "In this project, I'm responsible for maintaining the website's UI by implementing different frontend features using HTML, CSS and JavaScript. I\'m also responsible for displaying data retrived from the .NET backend.",
+    link: 'https://on-lineacademy.com/main/index.asp',
+  },
+  {
+    iconUrl: player,
+    theme: 'btn-back-pink',
+    name: 'ConX Agency',
+    description:
+      "As the tech lead of this project, I'm responsible for managing the system's backend using NestJS and Supabase. I'm also responsible for integrating the different endpoints within the frontend.",
+    link: 'https://conx-group.webflow.io/',
+  },
+  {
+    iconUrl: socialmedia,
+    theme: 'btn-back-orange',
+    name: 'Hotel Sentral',
+    description:
+      "In this project, I'm responsible for fixing various issues and implementing new features on the entire web system using HTML, CSS, React and NestJS (frontend and backend).",
+    link: 'https://www.hotelsentral.com.my/',
+  },
+  // {
+  //     iconUrl: consultation,
+  //     theme: 'btn-back-black',
+  //     name: 'HereForYou Consultation',
+  //     description: 'A full-stack university consultation web system that is buit using HTML, CSS, JavaScript and PHP.',
+  //     link: 'https://github.com/ConquerorFY/HereForYou-Consultation',
+  // },
+  // {
+  //     iconUrl: face,
+  //     theme: 'btn-back-yellow',
+  //     name: 'Face Swapper',
+  //     description: 'A web application that harnesses the power of computer vision to detect faces in photos and allows users to swap faces between different images.',
+  //     link: 'https://github.com/ConquerorFY/Face-Swapper',
+  // }
+];
---
+++ src/constants/socialLinks.js
@@ -0,0 +1,14 @@
+import { github, linkedin } from '../assets/icons';
+
+export const socialLinks = [
+  {
+    name: 'GitHub',
+    iconUrl: github,
+    link: 'https://github.com/ConquerorFY',
+  },
+  {
+    name: 'LinkedIn',
+    iconUrl: linkedin,
+    link: 'https://www.linkedin.com/in/ryan-lim-fang-yung-207412207/',
+  },
+];
